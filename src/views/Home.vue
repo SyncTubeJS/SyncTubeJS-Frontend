cc8586ebca93397b3baaf04602679cb0190c10f7
Joseph Richey <josephrichey@me.com>
2020-02-05 17:53:09 -0500

    Testing socket.io connection

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,32 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-    <button v-on:click="clickButton('yo yo yo test')">Agboghidi</button>
+    <button @click="clickButton()">Test Connection</button>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
 export default {
   name: 'home',
   components: {
-    HelloWorld
+
   },
   sockets: {
     connect() {
       console.log('socket connected')
     },
-    customEmit(val) {
-      console.log('this method was fired by the socket server. eg: io.emit("customEmit", data)')
+    console_message(val) {
+      console.log(val)
     }
   },
   methods: {
-    clickButton(val) {
-      // this.$socket.client is `socket.io-client` instance
-      this.$socket.emit('emit_method', val);
+    clickButton() {
+      this.$socket.emit('test_connect', { payload : 'test_payload' });
     }
   }
 }
