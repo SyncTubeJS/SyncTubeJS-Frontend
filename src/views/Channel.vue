1a133ccea89cc407c7e3143bdf4f791f70b0651a
Joseph Richey <josephrichey@me.com>
2020-02-06 12:41:45 -0500

    Small line break delete

--- src/views/Channel.vue
+++ src/views/Channel.vue
@@ -18,7 +18,6 @@ export default {
 
     // Grab the route slug and join a channel on Channel view mount
     let roomName = this.$route.params.channelSlug
-
     if(roomName) {
       this.$socket.emit('join_create_room', roomName)
     }
